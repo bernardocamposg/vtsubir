4ea336f039f55627cdb8ea383e5000000dea5178
Bernardo Adolfo <bernardoguajardo98@hotmail.com>
2017-11-24 16:31:29 -0600

    Trabajoparasubir

---
+++ PaginaProyecto/scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+ $('.slider').bxSlider({
+     mode: 'fade',
+     slideWidth: 1500,
+     captions: true,
+     auto: true,
+     autoControls:true,
+     stopAutoOneClick:true,
+     slieHeight: 600
+ }); 
+});
